64288ac30b6edd2480f6636e125557d4b9be8c96
USSL <mr.sakiulkawsar@gmail.com>
2025-12-06 13:23:08 +0600

    m

--- district_list.php
+++ district_list.php
@@ -2,5 +2,8 @@
 $districts = array(1=>["marsingid"," narayanjong"], 2=>["comilla", "noakhali"]);
 
 $id = $_POST['id'];
-print_r($districts[$id]);
+$lists = ($districts[$id]);
+foreach($lists as $list){
+    echo "<option>".$list."</option>";
+}
 ?>
