a95ff31452738bf0cb6e836d79cdfe02072dba66
sakiulkawsar <mr.sakiulkawsar@gmail.com>
2025-12-07 00:03:49 +0600

    m

--- district_list.php
+++ district_list.php
@@ -1,9 +1,12 @@
-<?php 
-$districts = array(1=>["marsingid"," narayanjong"], 2=>["comilla", "noakhali"]);
+<?php
+$districts = array(
+    1 => ["marsingid", " narayanjong"],
+    2 => ["comilla", "noakhali"],
+    3 => ["potia", "cox's bazar"]
+);
 
 $id = $_POST['id'];
 $lists = ($districts[$id]);
-foreach($lists as $list){
-    echo "<option>".$list."</option>";
+foreach ($lists as $list) {
+    echo "<option>" . $list . "</option>";
 }
-?>
